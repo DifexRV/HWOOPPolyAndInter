a3b5c1b68dafd2e4189a7e6a8dd0ce1ad110dc5d
Roman <roOmkin@gmail.com>
2023-09-24 21:31:54 +0700

    Мастер ветка по ООП - Полиморфизм и Интерфейсы.Вер.1

--- src/main/java/org/example/ServiceStation.java
+++ src/main/java/org/example/ServiceStation.java
@@ -1,25 +1,42 @@
 package org.example;
 
-public class ServiceStation {
+public class ServiceStation implements ServiceStationVehicle {
+
+    @Override
     public void check(Car car, Bicycle bicycle, Truck truck) {
         if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
+            System.out.println("Обслуживаем " + car.getModelName());
+            for (int i = 1; i <= car.getWheelsCount(); i++) {
+                this.updateTyre();
             }
-            car.checkEngine();
+            this.checkEngine();
         } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
+            System.out.println("Обслуживаем " + truck.getModelName());
+            for (int i = 0; i < truck.getWheelsCount(); i++) {
+                this.updateTyre();
             }
-            truck.checkEngine();
-            truck.checkTrailer();
+            this.checkEngine();
+            this.checkTrailer();
         } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
+            System.out.println("Обслуживаем " + bicycle.getModelName());
+            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+                this.updateTyre();
             }
         }
     }
+
+
+    private void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+
+    private void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    private void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
 }
---
+++ src/main/java/org/example/ServiceStationVehicle.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface ServiceStationVehicle {
+    void check(Car car, Bicycle bicycle, Truck truck);
+
+}
